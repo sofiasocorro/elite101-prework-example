e972403d978accb142705c352a615000d36819bf
sofiasocorro <socor512224@gmail.com>
2025-09-15 02:29:50 +0000

    update 9/14/2025 (9:30): created template for chatbot

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print('Welcome to Chatbot!')
-name = input("What's your name? ")
+print('Welcome to Elite 101 Chatbot!')
+name = input("Please enter your name: ")
+age = input('Greetings ' + name + '! how old are you? ')
+print('Welcome ' + name + '!' ' I remember when I was ' + age+ ', How can I help you today? \n')
+print('''Please choose from the following:
+1. Create an account
+2. Generate a table
+3. Sort numbers
+4. Exit ''')
+choice = input('Please enter your choice: ')
+if choice == '4':
+    print('Goodbye, ' + name+ '! Have a great day!')
