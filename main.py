ae70f7fe84b484b280b459f801c203c3b714604e
sofiasocorro <socor512224@gmail.com>
2025-11-10 14:48:45 +0000

    mainv3

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 '''
-FINANCE- BANK ACCOUNT
-ADD MONEY
-TRANSFER MONEY
-BALANCE
+
+CHATBOT: ELITE'S BANKING SYSTEM
 
 
 '''
 
 
 #WELCOME USER
-print("Welcome to Elite's Bank!")
+print(''''
+
+-------------------------
+
+Welcome to Elite's Bank!
+      
+-------------------------
+      
+''')
 
 #USER'S NAME
 name = input("Please enter your name: ")
@@ -17,11 +23,17 @@ age = input('Greetings ' + name + '! how old are you? ')
 print('Welcome ' + name + '!' ' I remember when I was ' + age+ ', How can I help you today? \n')
 
 #USER'S CHOICE
-print('''Please choose from the following:
+print('''
+-------------------------
+
+Please choose from the following:
 1. Check balance
 2. Add money
 3. Transfer money
-4. Exit ''')
+4. Exit 
+      
+-------------------------
+''')
 
 #SET GENERAL/SAVINGS BALANCE
 general_balance = 1500
@@ -35,17 +47,17 @@ while True:
         print('Savings balance: $', savings_balance)
 
 
-    if choice == '2':
+    elif choice == '2':
         account_choose = input('Which account do you want to add money to? General or savings: ' )
         revised_choice = account_choose.lower()
 
-        if account_choose == 'general':
+        if revised_choice == 'general':
             add_amount = input('How much money do you want to add to your general account? $')
             add_amount = float(add_amount)
             general_balance = general_balance + add_amount
             print('New general balance: $', general_balance)
 
-        if account_choose == 'savings':
+        elif revised_choice == 'savings':
             add_amount = input('How much money do you want to add to your savings account? $')
             add_amount = float(add_amount)
             savings_balance = savings_balance + add_amount
@@ -55,11 +67,11 @@ while True:
             correct_choice = input('Please choose between general or savings: ')
             
             
-    if choice == '3':
+    elif choice == '3':
         account_choose = input('Which account do you want to transfer money to? General or savings: ' )
         revised_choice = account_choose.lower()
 
-        if account_choose == 'general':
+        if revised_choice == 'general':
             transfer_amount = input('How much money do you want to transfer to your general account from savings? $')
             transfer_amount = float(transfer_amount)
             savings_balance = savings_balance - transfer_amount
@@ -68,7 +80,7 @@ while True:
             print('New savings balance: $', savings_balance)
             
 
-        if account_choose == 'savings':
+        elif revised_choice == 'savings':
             transfer_amount = input('How much money do you want to transfer to your savings account from general? $')
             transfer_amount = float(transfer_amount)
             savings_balance = savings_balance + transfer_amount
@@ -77,13 +89,13 @@ while True:
             print('New savings balance: $', savings_balance)
 
         else:
-            correct_choice = input('Please choose between general or savings: ')
+            correct_choice = input('Please only choose between general or savings: ')
 
-    if choice == '4':
+    elif choice == '4':
         print('Goodbye, ' + name+ '! Have a great day!')
         break
     
     else:
-        fix = input('Please type 1, 2, 3, or 4: ')
+        fix = input('Sorry, please choose only: 1, 2, 3, or 4 ')
 
     
